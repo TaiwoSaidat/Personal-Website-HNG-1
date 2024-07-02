3ba0f15483067106a17303386ae23cd568397c51
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2024-07-02 11:06:13 +0100

    feat: update day and date

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function updateDateTime() { 
     const now = new Date(); 
     const currentTimeUTC = now.toUTCString().slice(17, 25); // Extract UTC time in 24-hour format 
-    const currentDay = now.toLocaleDateString('en-US', { weekday: 'long' }); 
+    const currentDay = now.toLocaleDateString('en-US', { weekday: 'long', day: 'numeric', month: 'long' });  
   document.querySelector('[data-testid="currentTimeUTC"]').textContent = currentTimeUTC; 
     document.querySelector('[data-testid="currentDay"]').textContent = currentDay; 
   } 
   // Initial update 
   updateDateTime(); 
   // Update every second 
-  setInterval(updateDateTime, 1000); 
+  setInterval(updateDateTime, 1000);  
