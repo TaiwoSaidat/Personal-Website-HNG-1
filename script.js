bfc2c6ad7fd17d57cde5c7ddca21dfd3663c7d53
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2024-07-02 10:51:21 +0100

    feat: update html, css and javascript code

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
-class sample {
-    constructor(trial) {
-        this.trial = trial; 
-    }
-    
-    start() {
-    console.log(this.trial + " is starting")
-    }
-}
-
-let mySample = new sample("TaiwoSaidat")
-
-mySample.start();
+function updateDateTime() { 
+    const now = new Date(); 
+    const currentTimeUTC = now.toUTCString(); 
+    const currentDay = now.toLocaleDateString('en-US', { weekday: 'long' }); 
+  document.querySelector('[data-testid="currentTimeUTC"]').textContent = currentTimeUTC; 
+    document.querySelector('[data-testid="currentDay"]').textContent = currentDay; 
+  } 
+  // Initial update 
+  updateDateTime(); 
+  // Update every second 
+  setInterval(updateDateTime, 1000); 
